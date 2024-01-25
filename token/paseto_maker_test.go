0f97ae3e6a9c57ec4e124206e7da1db29c9b2e69
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2024-01-25 02:58:31 +0100

    done

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,44 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/Owoade/go-bank/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPacetoMaker(t *testing.T) {
+
+	user := int64(1)
+
+	sk := utils.GenerateRandomString(32)
+
+	pasetoMaker, _ := NewPasetomaker(sk)
+
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := pasetoMaker.CreateToken(user, duration)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := pasetoMaker.VerifyToken(token)
+	require.NoError(t, err)
+	require.Equal(t, payload.UserId, user)
+
+	// Define the layout that matches the datetime string
+	layout := "2006-01-02 15:04:05.9999999 -0700 MST m=-1.020304"
+	_issuedAt, _ := time.Parse(layout, issuedAt.String())
+	__issuedAt, _ := time.Parse(layout, payload.IssuedAt)
+
+	_expiredAt, _ := time.Parse(layout, expiredAt.String())
+	__expireAt, _ := time.Parse(layout, expiredAt.String())
+
+	require.WithinDuration(t, _issuedAt, __issuedAt, time.Second)
+	require.WithinDuration(t, _expiredAt, __expireAt, time.Second)
+
+}
